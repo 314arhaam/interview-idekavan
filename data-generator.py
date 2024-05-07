a8ade9df566bbaf7cd0f1e7c0b5c7666e6a4e013
Parham Abbasi <prmbas@gmail.com>
2024-05-07 20:31:17 +0330

    minor fixes

--- data-generator.py
+++ data-generator.py
@@ -2,12 +2,6 @@ import pandas as pd
 import datetime
 import random
 
-products = [
-    "PS5",
-    "BMW 328i",
-    "AMD Ryzen 9 7950X3D",
-    "Potato"
-]
 
 start_day = datetime.datetime(year = 2023,
                               month = 10,
@@ -20,9 +14,9 @@ n_data = 200
 
 
 
-with open('schema.txt', 'w') as schema_file:
-    schema_file.write('-- POSTGRESQL | ORDERS SCHEMA')
-    schema_file.write('''
+with open('schema_orders.txt', 'w') as orders_schema_file:
+    orders_schema_file.write('-- POSTGRESQL | ORDERS SCHEMA')
+    orders_schema_file.write('''
 create table orders(
     id int PRIMARY KEY,
     product_id int,
@@ -30,7 +24,7 @@ create table orders(
     num int
     );
     ''')
-    schema_file.write('insert into orders (id, product_id, order_date, num)\nVALUES\n')
+    orders_schema_file.write('insert into orders (id, product_id, order_date, num)\nVALUES\n')
     sep = ', '
     for i in range(n_data):
         if i == n_data - 1:
@@ -39,4 +33,4 @@ create table orders(
         random_id = random.randint(1, len(products))
         random_date = start_day + datetime.timedelta(days = random.randint(0, n_days))
         random_date = str(random_date).split()[0]
-        schema_file.write(f'    ({i+1}, {random_id}, \'{random_date}\', {random.randint(1, 50)}){sep}\n')
+        orders_schema_file.write(f'    ({i+1}, {random_id}, \'{random_date}\', {random.randint(1, 50)}){sep}\n')
